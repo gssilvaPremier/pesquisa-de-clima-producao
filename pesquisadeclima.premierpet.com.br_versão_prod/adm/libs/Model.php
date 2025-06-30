880af9667f7ebf8b3a4a4987860b196185a8c104
gssilva <gssilva@premierpet.com.br>
2025-06-30 11:41:47 -0300

    versao producao pesquisa de cilma

--- pesquisadeclima.premierpet.com.br_versão_prod/adm/libs/Model.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Model {
-
-	function __construct() {
-		$this->db = new Database();
-		$this->db->exec("SET NAMES 'utf8'");
-	}
-}
--- pesquisadeclima.premierpet.com.br_versão_prod/adm/views/themes/bootstrap/default/dashboard/index.php
+++
@@ -1,8 +0,0 @@
-<div style="display:none;">
-  <h1>Painel Princial</h1>
-  <form id="randomInsert" action="<?php echo URL . "dashboard/xhrInsert"; ?>" method="post">
-    <input type="text" name="text" />
-    <input type="submit" disabled  />
-  </form>
-  <div id="listInserts"></div>
-</div>
